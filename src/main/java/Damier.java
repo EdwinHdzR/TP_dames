425d37180376eaecae3d9c472a45fe5c757f5e57
EdwinHdzR <113386735+EdwinHdzR@users.noreply.github.com>
2023-01-05 08:44:54 +0100

    Creacion del repositorio

---
+++ src/main/java/Damier.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class Damier {
+    private int[][] tableau;
+
+    public Damier(){
+        this.tableau = new int[10][10];
+    }
+    public Damier(int[][] tableau) {
+        this.tableau = tableau;
+    }
+
+    public int[][] getTableau() {
+        return tableau;
+    }
+
+    public void setTableau(int[][] tableau) {
+        this.tableau = tableau;
+    }
+
+    public void initDamier(){
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i) {
+
+                }
+
+            }
+
+        }
+    }
+}
